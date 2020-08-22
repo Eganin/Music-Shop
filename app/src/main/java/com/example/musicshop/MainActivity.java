f391566e6e0cf0932d880f51ab8d9514a5d42e65
Eganin <egorzaharin12@gmail.com>
2020-08-22 21:46:13 +0500

    add set image and refactoring

--- app/src/main/java/com/example/musicshop/MainActivity.java
+++ app/src/main/java/com/example/musicshop/MainActivity.java
@@ -3,12 +3,143 @@ package com.example.musicshop;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+
+// используем interface AdapterView.OnItemSelectedListener чтобы прослушать Spinner
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+    private static Integer quantity;
+    private static final String NULL_RESULT = "0";// нулевое значение
+    private String[] nameGoods = new String[]{"guitar", "drums", "keyboard"};
+    private Double[] goodsPrice = new Double[]{500.0D, 1000.0D, 1500.0D};
+    String goodsName;
+    double price;
+    Spinner spinner;
+    ArrayList<String> spinnerArrayList;
+    ArrayAdapter<String> spinnerAdapter;
+    HashMap<String, Double> goodsMap;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        fillingSpinner();
+        fillingMap();
+
+    }
+
+    public void increaseQuantity(View view) {
+        /*
+        прибавляем значение к textview
+         */
+        TextView textViewCounter = findViewById(R.id.textViewCounter);// находим по id
+        String textCounter = (String) textViewCounter.getText(); // получаем текст
+        quantity = Integer.parseInt(textCounter);
+        quantity++;
+        String result = String.valueOf(quantity);
+        textViewCounter.setText(result); // вставляем текст
+        TextView textViewPrice = findViewById(R.id.textViewPrice);
+        String resultSum = String.valueOf(price * quantity);
+        textViewPrice.setText(resultSum);
+    }
+
+    public void addingQuantity(View view) {
+        /*
+        уменьшаем  значение у textview
+         */
+        TextView textViewCounter = findViewById(R.id.textViewCounter);
+        String textCounter = (String) textViewCounter.getText();
+        quantity = Integer.parseInt(textCounter);
+        if (quantity > 0) {// чтобы нельзя было уйти в минус
+            quantity--;
+            String result = String.valueOf(quantity);
+            textViewCounter.setText(result);
+            TextView textViewPrice = findViewById(R.id.textViewPrice);
+            String resultSum = String.valueOf(price * quantity);
+            textViewPrice.setText(resultSum);
+        } else {
+            textViewCounter.setText(NULL_RESULT);
+        }
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        /*
+        Метод который срабатывает на нажатие объекта из выпадающего списка - spinner
+         */
+        TextView textViewCounter = findViewById(R.id.textViewCounter);// находим по id
+        String textCounter = (String) textViewCounter.getText(); // получаем текст
+        // выбранное значение в spinner на данный момент
+        goodsName = spinner.getSelectedItem().toString();
+        price = (double) goodsMap.get(goodsName);
+        int quantity = Integer.parseInt(textCounter);
+        TextView textViewPrice = findViewById(R.id.textViewPrice);
+        String result = String.valueOf(price * quantity);
+        textViewPrice.setText(result);
+
+        // при нажатии элемента в списке выбираем соответсвующее изображение
+        setImageItem();
+    }
+
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+
+    private void setImageItem(){
+        ImageView imageView = findViewById(R.id.imageItem);
+        switch (goodsName) {
+            case "guitar":
+                // установка изображения из папки drawable
+                imageView.setImageResource(R.drawable.guitar);
+                break;
+
+            case "drums":
+                imageView.setImageResource(R.drawable.drum);
+                break;
+
+            case "keyboard":
+                imageView.setImageResource(R.drawable.piano);
+                break;
+        }
+    }
+
+    private void fillingSpinner() {
+        /*
+        наполнение spinner значением
+         */
+        spinner = findViewById(R.id.choiceSpinner);
+        spinner.setOnItemSelectedListener(this);// активировать прослушыватель событий
+        spinnerArrayList = new ArrayList<>(3);// содержимое списка spinner
+        Collections.addAll(spinnerArrayList, nameGoods);// добавляем в spinnerArrayList массив
+        //init adapter to spinner
+        spinnerAdapter = new ArrayAdapter<>(this,//class
+                android.R.layout.simple_spinner_item,//const
+                spinnerArrayList);//collection
+
+        // устанавливаем выпадающий список
+        spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(spinnerAdapter); // add Adapter
+    }
+
+    private void fillingMap() {
+        /*
+        наполнение map стоимостью и названием товара
+         */
+        goodsMap = new HashMap<>();
+        for (int i = 0; i < goodsPrice.length; i++) {
+            goodsMap.put(nameGoods[i], goodsPrice[i]);
+        }
     }
 }
